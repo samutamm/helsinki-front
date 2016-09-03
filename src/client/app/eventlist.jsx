5f1d57e353b2fb262b6f5de4f132f170bc94befe
samutamm <samutamm@gmail.com>
2016-09-03 22:05:57 +0300

    Server and listing functionality.

--- src/client/app/eventlist.jsx
+++ src/client/app/eventlist.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import $ from "jquery";
+
+const URL = "http://localhost:5000/events";
+//const URL = "https://www.npmjs.com/package/react-fetch";
 
 class Event extends React.Component {
   constructor(props) {
@@ -6,23 +10,47 @@ class Event extends React.Component {
   }
   //<div onClick={this.handleClick.bind(this)}></div>
   render() {
-    return (
-      <div>
-        <p>Eventum!</p>
-      </div>
-    );
+
+      return (<div><p>Tuntematon</p></div>);
+    
+
   }
 }
+
 
 class EventsList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      events: []
+    }
+  }
+  componentDidMount() {
+    $.ajax({
+      url: URL,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        const resultJson = JSON.parse(data)
+        this.setState({
+          events: resultJson
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(URL, status, err.toString());
+      }.bind(this)
+    });
   }
   render() {
+    var eventNodes = this.state.events.map(function(event) {
+      return (
+        <Event data={event} key={event._id.$oid}>
+        </Event>
+      );
+    });
     return (
       <div>
-        <Event />
-        <Event />
+        {eventNodes}
       </div>
     );
   }
